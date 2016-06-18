10cb3da60b6d6f5533e5ad7a00a371573498b54a
Baptiste CHALEIL <bchaleil@e1r9p15.42.fr>
2016-06-18 20:17:34 +0200

    Save for home

--- category_controller.php
+++ category_controller.php
@@ -1,5 +1,6 @@
 <?php
 include "data/create.php";
+include "data/delete.php";
 if ($_POST["method"] == "create") {
 	create_category($_POST["name"]);
 	header("Location: dashboard.php?content=category");
@@ -7,9 +8,9 @@ if ($_POST["method"] == "create") {
 	edit_category($_GET["old_name"], $_GET["new_name"]);
 	header("Location: dashboard.php?content=category");
 } else if ($_GET["method"] == "delete" && $_GET["name"]) {
-	print_r($_GET);
-	//delete_category($_GET["name"]);
-	//header("Location: dashboard.php?content=category");
+//	print_r($_GET);
+	delete_category($_GET["name"]);
+//	header("Location: dashboard.php?content=category");
 } else {
 	//page 404
 }
--- data/delete.php
+++ data/delete.php
@@ -29,13 +29,19 @@ function delete_category($name) {
 		if ($category[$i]["name"] == $name) {
 			array_splice($category, $i, $i);
 			for ($j = 0; $j < count($products); $j++) {
-				for ($k = 0; $k < count($products); $k++) {
-					
+				for ($k = 0; $k < count($products[$j]["category"]); $k++) {
+					if ($products[$j]["category"][$k] == $name) {
+						unset($products[$j]["category"][$k]);
+					}
+					$products[$j]["category"] = array_values($products[$j]["category"]);
 				}
-				if ($product["category"]
 			}
 		}
 	}
-	file_put_contents("data/users", serialize($users));
+	print_r($category);
+	echo "<br>";
+	print_r($products);
+	file_put_contents("data/products", serialize($products));
+	file_put_contents("data/category", serialize($category));
 }
 ?>
